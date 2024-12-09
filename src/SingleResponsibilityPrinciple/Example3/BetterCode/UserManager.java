2a12ca5ded9be873e7298fc0285669534ca0007f
Aditya <adityamgs2018@gmail.com>
2024-12-09 23:52:54 +0530

    Example3

---
+++ src/SingleResponsibilityPrinciple/Example3/BetterCode/UserManager.java
@@ -0,0 +1,12 @@
+package Lesson1_SingleResponsibilityPrinciple.Example4.ProblematicCode;
+
+public class UserManager {
+    public void createUser(String username) {
+        // Code to create a user
+    }
+
+    public void deleteUser(String username) {
+        // Code to delete a user
+    }
+
+}
